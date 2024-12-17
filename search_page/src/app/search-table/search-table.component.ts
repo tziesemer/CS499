ffe7bb49e3fc941723374ba29f47e3976350e1fb
tziesemer <52858924+tziesemer@users.noreply.github.com>
2024-12-17 12:33:13 -0500

    Update search-table.component.ts
    added commits, got rid of test data that wasn't being utilized

--- search_page/src/app/search-table/search-table.component.ts
+++ search_page/src/app/search-table/search-table.component.ts
@@ -2,7 +2,6 @@ import { AfterViewInit, Component, ViewChild } from '@angular/core';
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
 import { MatSort, Sort, MatSortModule } from '@angular/material/sort';
-import { rescues } from '../data/testData';
 import { CommonModule } from '@angular/common';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatSelectModule } from '@angular/material/select';
@@ -20,10 +19,11 @@ import { SearchMapComponent } from '../search-map/search-map.component';
 })
 
 export class SearchTableComponent implements AfterViewInit {
-  rescues: Array<any> = rescues;
-
+ 
+//Pulls in rescue data from rescue_outcomes
   realRescues: Array<any> = rescue_outcomes;
 
+  // Sets a starting position based on the pulled data for the search-map component
   startPosition = this.realRescues[0];
   position = {
     lat: this.startPosition.location_lat,
@@ -31,24 +31,27 @@ export class SearchTableComponent implements AfterViewInit {
   };
 
   markerName = this.startPosition.name;
-
+//This controls which coloums are shown in the html table
   displayedColomns: string[] = ['select','name', 'animal_type', 'age_upon_outcome', 'breed', 'sex_upon_outcome', 'outcome_type', 'outcome_subtype'];
-
+//This sets the data source for the table
   dataSource = new MatTableDataSource(this.realRescues);
 
   @ViewChild(MatPaginator) paginator: MatPaginator;
   @ViewChild(MatSort) sort: MatSort; 
 
   constructor(
+    //This creates the filter service so it can be utilized later
     private filterService: FiltersService
   ) {}
-
+//Function activated by the radio buttons in the table. Updates the map based on which row is selected
   public thisSelected(value): void {
     let newMarker = {
       lat: value.location_lat,
       lng: value.location_long
     }
     this.position = newMarker;
+
+    //If name is empty then the markerName will just display No name rather than being empty or showing no data
     if(value.name != ""){
     this.markerName = value.name;
     }
@@ -56,10 +59,11 @@ export class SearchTableComponent implements AfterViewInit {
       this.markerName = "No name";
     }
   }
-
+//Function activated whenever a filter radio button is selected to call the filter function from the filter service
   public isFiltered(value): void {
     
     let finalData = this.filterService.filterData(this.realRescues, value);
+    //Resets table data to mach filtered data
     this.dataSource.data = finalData;
   }
 
