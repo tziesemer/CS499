182bc2dd273bfda7010498afcbaf55acbccc16f5
tziesemer <52858924+tziesemer@users.noreply.github.com>
2024-12-20 22:31:06 -0500

    Update app.routes.ts

--- search_page/src/app/app.routes.ts
+++ search_page/src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
 import { AddRescueComponent } from './add-rescue/add-rescue.component';
 import { EditRescueComponent } from './edit-rescue/edit-rescue.component';
+
 export const routes: Routes = [
     {path: 'list-rescues', component: SearchTableComponent},
     { path: 'register', component: RegisterComponent},
