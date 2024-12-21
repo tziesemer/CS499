73de63b97c90cde15f913dbad6c5fd5c84f603ce
tziesemer <52858924+tziesemer@users.noreply.github.com>
2024-12-20 22:50:36 -0500

    Update authentication.js
    added comments

--- app_api/controllers/authentication.js
+++ app_api/controllers/authentication.js
@@ -11,10 +11,13 @@ const register = async(req, res) => {
             .json({"message": "All fields required"});
     }
 
+    //Fills in user data to create user object
     const user = new User();
     user.name = req.body.name;
     user.email = req.body.email;
+    //Calls setpassword to obscure saved password
     user.setPassword(req.body.password);
+    //makes async call to save user to the database and then generate the JWT token for session authorization.
     const q = await user.save();
         if (!q) {
             res
@@ -28,6 +31,7 @@ const register = async(req, res) => {
         }
 };
 
+//Login will compare supplied user with user database and create a new JWT token if use authenticates
 const login = (req, res) => {
     if (!req.body.email || !req.body.password) {
         return res
